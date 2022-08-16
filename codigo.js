e5394a71dc15881aa028e510be736c304e4f92d7
fabianlizana1870@gmail.com <fabianlizana1870>
2022-08-16 16:52:30 -0400

    actualizando

--- codigo.js
+++ codigo.js
@@ -5,7 +5,7 @@ function encriptar() {
     var txtcifrado = txtcifrado.replace(/i/igm,"imes");
     var txtcifrado = txtcifrado.replace(/a/igm,"ai");
     var txtcifrado = txtcifrado.replace(/u/igm,"ufat");
-    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("imgDerecha").style.display = "none";
     document.getElementById("texto").style.display = "none";
     document.getElementById("copiar").style.display = "show";
     document.getElementById("texto2").innerHTML = txtcifrado;
@@ -20,7 +20,7 @@ function encriptar() {
     var txtcifrado = txtcifrado.replace(/imes/igm,"i");
     var txtcifrado = txtcifrado.replace(/ai/igm,"a");
     var txtcifrado = txtcifrado.replace(/ufat/igm,"u");
-    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("imgDerecha").style.display = "none";
     document.getElementById("texto").style.display = "none";
     document.getElementById("copiar").style.display = "show";
     document.getElementById("texto2").innerHTML = txtcifrado;
